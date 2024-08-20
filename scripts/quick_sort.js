4fbcc63e71bb5b4a704259051ba7e0ba95ad0d9d
Abhuday Lath <abhudaylath@gmail.com>
2024-08-21 05:21:43 +0530

    small changes

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -1,43 +1,58 @@
-function partition(start, end) {
-    var pivotValue = divSizes[start]
-    var i = start + 1
-    div_update(divElements[start], divElements[start], divSizes[start], divSizes[start], colors.comparisons);
+function quick_partition(start, end) {
+    var i = start + 1;
+    var piv = divSizes[start];
+    div_update(divElements[start], divElements[start], divSizes[start], divSizes[start], "yellow");//Color update
+
     for (var j = start + 1; j <= end; j++) {
-        if (divSizes[j] < pivotValue) {
-            div_update(divElements[j], divElements[j], divSizes[j], divSizes[j], colors.comparisons);
-            div_update(divElements[i], divElements[j], divSizes[i], divSizes[j], colors.swap);
+        if (divSizes[j] < piv) {
+            div_update(divElements[j], divElements[j], divSizes[j], divSizes[j], "yellow");//Color update
+
+            div_update(divElements[i], divElements[j], divSizes[i], divSizes[j], "red");//Color update
+            
             var temp = divSizes[i];
             divSizes[i] = divSizes[j];
             divSizes[j] = temp;
-            div_update(divElements[i], divElements[j], divSizes[i], divSizes[j], colors.swap);
-            div_update(divElements[i], divElements[j], divSizes[i], divSizes[j], colors.default);
-            i++;
+
+            div_update(divElements[i], divElements[j], divSizes[i], divSizes[j], "red");//Height update
+            
+            div_update(divElements[i], divElements[j], divSizes[i], divSizes[j], "blue");//Height update
+            
+            i += 1;
         }
     }
-    div_update(divElements[start], divElements[i - 1], divSizes[start], divSizes[i - 1], colors.swap);
-    var temp = divSizes[i - 1];
-    divSizes[i - 1] = divSizes[start];
-    divSizes[start] = temp;
-    div_update(divElements[start], divElements[i - 1], divSizes[start], divSizes[i - 1], colors.default);
+    div_update(divElements[start], divElements[i - 1], divSizes[start], divSizes[i - 1], "red");//Color update
+    
+    var temp = divSizes[start];//put the pivot element in its proper place.
+    divSizes[start] = divSizes[i - 1];
+    divSizes[i - 1] = temp;
+
+    div_update(divElements[start], divElements[i - 1], divSizes[start], divSizes[i - 1], "red");//Height update
+    
     for (var t = start; t <= i; t++) {
-        div_update(divElements[t], divElements[t], divSizes[t], divSizes[t], colors.sorted)
+        div_update(divElements[t], divElements[t], divSizes[t], divSizes[t], "green");//Color update
     }
+
     return i - 1;
 }
 
-function Sort(start, end) {
-    if (start >= end) return;
-    var pivotIndex = partition(start, end);
-    Sort(start, pivotIndex - 1);
-    Sort(pivotIndex + 1, end);
+function quick_sort(start, end) {
+    if (start < end) {
+        var piv_pos = quick_partition(start, end);
+        quick_sort(start, piv_pos - 1);
+        quick_sort(piv_pos + 1, end);
+    }
 }
 
 function QuickSort() {
-    document.getElementById("Time_Worst").innerHTML = "O(N^2)";
-    document.getElementById("Time_Best").innerHTML = "O(NLOG(N))";
-    document.getElementById("Time_Average").innerHTML = "O(NLOG(N))";
-    document.getElementById("Space_Worst").innerHTML = "O(LOG(N))";
+    document.getElementById("Time_Worst").innerText = "O(N^2)";
+    document.getElementById("Time_Average").innerText = "Θ(N log N)";
+    document.getElementById("Time_Best").innerText = "Ω(N log N)";
+
+    document.getElementById("Space_Worst").innerText = "O(log N)";
+
     c_delay = 0;
-    Sort(0, divSizes.length - 1);
+
+    quick_sort(0, divSizes.length - 1);
+
     enableButtons();
 }
